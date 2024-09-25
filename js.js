ad40a08de92c7fce1293566ef4dd57bc1e55095f
Yohann vanherzecke <djbk62@gmail.com>
2024-09-25 15:23:30 +0200

    yo

--- js.js
+++ js.js
@@ -20,32 +20,55 @@ boutons.forEach(bouton => {
         switch (boutonId) {
             case 'btn-1':
                 console.log('cas pour  mode nuit ');
+
+
+
                 break;
             case 'btn-2':
                 console.log('carre a faire par max ');
+
+
+
                 break;
             case 'btn-3':
                 console.log('chat a faire par flo ');
+
+
+
                 break;
             case 'btn-4':
                 console.log('cygne a faire par max ');
+
+
+
                 break;
             case 'btn-5':
                 console.log('canard a faire par flo');
+
+
+
                 break;
             case 'btn-6':
                 console.log('coeur a faire par flo');
+
+
+
                 break;
             case 'btn-7':
                 console.log('tortue a faire par yo');
                 break;
+
+
+
             case 'btn-8':
                 console.log('bateau a faire par yo');
                 break;
-            case 'btn-9':
+          
+                case 'btn-9':
                 console.log('mystere max a faire par max ');
                 break;
-            case 'btn-10':
+           
+                case 'btn-10':
                 console.log('myster flo a faire par flo');
                 break;
                 case 'btn-11':
